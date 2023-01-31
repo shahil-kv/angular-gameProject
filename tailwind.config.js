3b9a0e1b12c2e187397af5e37fb1def0dc1b919d
shahil-leo <shahilcode@gmail.com>
2023-01-31 15:16:09 +0530

    added the video file for the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      'shahil': ['Roboto'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ["Open Sans"]
+    },
     extend: {},
   },
   plugins: [],
