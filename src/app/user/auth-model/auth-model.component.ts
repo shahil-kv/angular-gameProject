d4e12c6f262839994345e2b5eb0839c85145fbb2
shahil-leo <shahilcode@gmail.com>
2023-01-31 16:29:29 +0530

    added the auth model or the login and register in the model component insie the user module

---
+++ src/app/user/auth-model/auth-model.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-model',
+  templateUrl: './auth-model.component.html',
+  styleUrls: ['./auth-model.component.scss']
+})
+export class AuthModelComponent {
+
+}
