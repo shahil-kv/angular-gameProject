f00725cf212199f83fba2b9f867c8a7c5370686b
shahil-leo <shahilcode@gmail.com>
2023-01-31 16:30:20 +0530

    added the module inside the srcapp and the component inside the module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModelComponent } from './auth-model/auth-model.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthModelComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    AuthModelComponent
   ]
 })
 export class UserModule { }
