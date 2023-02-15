7f86d4a738ca20e6e15e8250783fcc6b59f6aa8c
shahil <shahilcode@gmail.com>
2023-02-16 00:56:06 +0530

    added the form and form controls object into the registeration

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  /**
+   * we are creating a reactive form inside the register form and we created a new object for that
+   */
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl('')
+  })
 }
