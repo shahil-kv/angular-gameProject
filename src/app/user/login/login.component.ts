7eb3e6fd5d21fd0ebee88f791ffbb6ff551b1e8a
shahil <shahilcode@gmail.com>
2023-02-17 20:36:09 +0530

    added the object for 2 way binding

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  credentials = {
+    email: '',
+    password: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
