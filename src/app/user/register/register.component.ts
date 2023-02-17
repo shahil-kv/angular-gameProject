aa92bd427b9b0d78bfa28c4dda50520485c5d896
shahil <shahilcode@gmail.com>
2023-02-17 12:31:35 +0530

    added the validation for the age in the register ts file

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -6,19 +6,39 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+/**
+ * we are creating a reactive form inside the register form and we created a new object for that
+ * ? we are creating new form in the register template file and
+ * * Form control is used for managing the state in the form like valid  from a control element
+ */
+
 export class RegisterComponent {
 
-  /**
-   * we are creating a reactive form inside the register form and we created a new object for that
-   */
-  registerForm = new FormGroup({
-    name: new FormControl('', [
+  name = new FormControl('', [Validators.required, Validators.minLength(3)])
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('',
+    [
       Validators.required,
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl('')
+      /**
+        * * The validator min age used  to sign in the website age more than 18
+        * * the validator max for max age in the website
+       *  */
+      Validators.min(18),
+      Validators.max(120)
+    ]);
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('')
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
   })
+
+
 }
