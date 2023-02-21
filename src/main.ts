0d3fbcf2f1d2880c0d9324f674e938ec8b954fbe
shahil <shahilcode@gmail.com>
2023-02-21 12:22:49 +0530

    added the loading for the firebase first then angular because of showing the login/and register imediately

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import firebase from 'firebase/compat/app'
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import 'firebase/compat/auth'
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+firebase.initializeApp(environment.firebase)
+
+let appInit = false
+
+firebase.auth().onAuthStateChanged(() => {
+
+  if (!appInit) {
+
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  }
+  appInit = true
+})
+
