a08b1a920b8de4ec44e653110d990161428451d2
shahil <shahilcode@gmail.com>
2023-02-21 12:20:45 +0530

    added the login and register button comes only when the user is not logged in using the observable submitting we will get the in boolean and we can check it

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
-
+import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -8,7 +8,10 @@ import { ModalService } from '../services/modal.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(public modal: ModalService) { }
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
