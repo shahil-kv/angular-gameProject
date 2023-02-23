623526bf083315766282850685ed75f03634eb0a
shahil <shahilcode@gmail.com>
2023-02-23 15:08:41 +0530

    added the routing for the folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+// we must have to use this routing  inorder to have the routing capacity in
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './video/manage/manage.component';
 
-const routes: Routes = [];
+// this is for the routing in the array
+const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'about' },
+  { path: 'manage', component: ManageComponent }
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
