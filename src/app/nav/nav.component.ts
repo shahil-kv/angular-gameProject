37eade1161c2efee66526ada68ea42258822c532
shahil <shahilcode@gmail.com>
2023-02-23 15:10:03 +0530

    added the router link for the navlink

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,7 +12,9 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService
+    public auth: AuthService,
+    private afAuth: AngularFireAuth,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -20,6 +24,18 @@ export class NavComponent implements OnInit {
     $event.preventDefault()
 
     this.modal.toggleModal('auth')
+  }
+
+  async logout($event: Event) {
+    if (confirm('Are you sure want to logout')) {
+      $event.preventDefault()
+      await this.afAuth.signOut()
+      await this.router.navigateByUrl('/')
+    } else {
+      $event.preventDefault()
+      console.log('sui')
+    }
+
   }
 
 }
