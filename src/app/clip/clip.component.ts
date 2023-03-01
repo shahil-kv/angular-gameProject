981e3141d5725323a355577fd07c9a2bd2cee22a
shahil <shahilcode@gmail.com>
2023-03-01 11:05:44 +0530

    doing routing

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clip',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClipComponent implements OnInit {
 
-  constructor() { }
+  id = '';
 
+  constructor(public route: ActivatedRoute) { }
+
+
+  // the route.params is an observable and it will emit the changes in the routing
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id
+      console.log(this.id);
+    })
   }
 
 }
