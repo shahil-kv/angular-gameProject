b78651fe379f6fcea29867a9dc31d6a638d04ff3
shahil <shahilcode@gmail.com>
2023-03-01 13:19:38 +0530

    added the query parameters for the manage component

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+//  after we imported the activatedRoute and we will get all the information about the route parameters in the router
 
 @Component({
   selector: 'app-clip',
@@ -15,9 +16,19 @@ export class ClipComponent implements OnInit {
 
   // the route.params is an observable and it will emit the changes in the routing
   ngOnInit(): void {
+    /***
+     * ? This code will also gives us that id but not as an observable so it will have drawbacks
+     * ? and the draws backs of the method is
+     * ! problems it is not updating when router is updated
+     * ? the biggest drawbacks are the clips example is when we click on a href then it url will change but it will not change the page title or dynamically changes things it will change the url but not the
+     * * using the next method will make you render easily when the url changes dynamically it effects everywhere in the website
+     */
+    // todo this.id = this.route.snapshot.params.id
+    console.log(this.id)
     this.route.params.subscribe((params: Params) => {
+      // we will get the information about the routing parameters using the observable given by the activated route
       this.id = params.id
-      console.log(this.id);
+
     })
   }
 
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router'
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  videoOrder = '1'
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+    })
   }
+  sort($event: Event) {
+    const { value } = ($event.target as HTMLSelectElement)
+    // ? we can use this method for simple task and rendering page using query parameters but if you need a powerful query parameters then we need to change the method
+    // todo  this.router.navigateByUrl(`/manage?sort=${value}`)
+    this.router.navigate([], {
+      // we are giving that the relative will be the current route and the query params will be the sort
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+
+  }
+
 
 }
