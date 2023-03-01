2ebd907d8d76bb7382a309f0930a277bf9d90c6f
shahil <shahilcode@gmail.com>
2023-03-01 13:26:15 +0530

    added the routing wild card for the page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ManageComponent } from './video/manage/manage.component';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 // this is for the routing in the array
 const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'about', component: AboutComponent, title: 'about' },
-  { path: 'clip/:id', component: ClipComponent, title: 'clip' }
+  { path: 'clip/:id', component: ClipComponent, title: 'clip' },
+  { path: '**', component: NotFoundComponent }
   // we are giving any values as our need in that id and then the id is like that and the id is stored in the router module we can access the routing parameter like the id
   // how we can retrieve data from the url using the router
+  // * the 2 stars in the routing is the wild card means if we have any unknown routes anything will show this component
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
     AboutComponent,
-    ClipComponent
+    ClipComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
